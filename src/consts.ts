7a71d77b59ada1a335ab52adf5560f284527377f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-24 19:51:05 +0300

    feat: create const genres

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+export const genres: string[] = [
+  'All genres',
+  'Comedies',
+  'Crime',
+  'Documentary',
+  'Dramas',
+  'Horror',
+  'Kids & Family',
+  'Romance',
+  'Sci-Fi',
+  'Thrillers',
+];
