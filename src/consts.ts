b0ddaf24f6565243673597230d1c410e494bf10e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-07 20:05:06 +0300

    feat: create enums AppRoute and AuthorizationStatus, replae data structure const genres

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,58 @@
-export const genres: string[] = [
-  'All genres',
-  'Comedies',
-  'Crime',
-  'Documentary',
-  'Dramas',
-  'Horror',
-  'Kids & Family',
-  'Romance',
-  'Sci-Fi',
-  'Thrillers',
+export const genres: { name: string, path: string }[] = [
+  {
+    name: 'All genres',
+    path: 'all'
+  },
+  {
+    name: 'Comedies',
+    path: 'comedies'
+  },
+  {
+    name: 'Crime',
+    path: 'crime'
+  },
+  {
+    name: 'Documentary',
+    path: 'documentary'
+  },
+  {
+    name: 'Dramas',
+    path: 'dramas'
+  },
+  {
+    name: 'Horror',
+    path: 'horror'
+  },
+  {
+    name: 'Kids & Family',
+    path: 'fids-family'
+  },
+  {
+    name: 'Romance',
+    path: 'romance'
+  },
+  {
+    name: 'Sci-Fi',
+    path: 'sci-fi'
+  },
+  {
+    name: 'Thrillers',
+    path: 'thrillers'
+  },
 ];
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  MyList = '/mylist',
+  Film = '/films/:id',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id',
+  NotFound = '*',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
