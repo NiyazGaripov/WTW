1430e0974e3942f6d6c0f388a991e1c7fea96c75
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-10 21:19:10 +0300

    feat: add footer component in main component

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,7 @@
 import {SvgSprite} from '../../components/svg-sprite/svg-sprite';
 import {FilmCard} from '../../components/film-card/film-card';
 import {Catalog} from '../../components/catalog/catalog';
+import {Footer} from '../../components/footer/footer';
 
 export function Main(): JSX.Element {
   return (
@@ -9,6 +10,7 @@ export function Main(): JSX.Element {
       <FilmCard/>
       <div className="page-content">
         <Catalog/>
+        <Footer/>
       </div>
     </>
   );
