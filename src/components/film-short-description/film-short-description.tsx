2a10a735746196f409d9b25545b93d8aceb19982
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-13 21:46:23 +0300

    feat: create film-short-description component

---
+++ src/components/film-short-description/film-short-description.tsx
@@ -0,0 +1,21 @@
+import {Controls} from '../controls/controls';
+
+type Props = {
+  name: string,
+  genre: string,
+  releaseYear: string,
+}
+
+export function FilmShortDescription({name, genre, releaseYear}: Props): JSX.Element {
+  return (
+    <div className="film-card__desc">
+      <h2 className="film-card__title">{name}</h2>
+      <p className="film-card__meta">
+        <span className="film-card__genre">{genre}</span>
+        <span className="film-card__year">{releaseYear}</span>
+      </p>
+
+      <Controls/>
+    </div>
+  );
+}
