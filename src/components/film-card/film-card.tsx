fa38a1d4537ed7f84001a81ec8faf5cb9827abf1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-29 10:49:10 +0300

    refactor: replace markup to components in component FilmCard

--- src/components/film-card/film-card.tsx
+++ src/components/film-card/film-card.tsx
@@ -1,4 +1,6 @@
 import {Header} from '../header/header';
+import {FilmShortDescription} from '../film-short-description/film-short-description';
+import {FilmInfo} from '../film-info/film-info';
 
 export function FilmCard(): JSX.Element {
   return (
@@ -12,35 +14,16 @@ export function FilmCard(): JSX.Element {
       <Header/>
 
       <div className="film-card__wrap">
-        <div className="film-card__info">
-          <div className="film-card__poster">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
-          </div>
-
-          <div className="film-card__desc">
-            <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-            <p className="film-card__meta">
-              <span className="film-card__genre">Drama</span>
-              <span className="film-card__year">2014</span>
-            </p>
-
-            <div className="film-card__buttons">
-              <button className="btn btn--play film-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"/>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list film-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"/>
-                </svg>
-                <span>My list</span>
-                <span className="film-card__count">9</span>
-              </button>
-            </div>
-          </div>
-        </div>
+        <FilmInfo
+          name='The Grand Budapest Hotel'
+          posterImage='img/the-grand-budapest-hotel-poster.jpg'
+        >
+          <FilmShortDescription
+            name='The Grand Budapest Hotel'
+            genre='Drama'
+            releaseYear='2014'
+          />
+        </FilmInfo>
       </div>
     </section>
   );
