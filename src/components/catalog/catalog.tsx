8d4afaa74d2c229f016e7dad66fe3c8a7d72dc5e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-30 09:22:35 +0300

    refactor: add prop className and condition for second title

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -2,12 +2,19 @@ import {PropsWithChildren} from 'react';
 import {films} from '../../consts';
 import {SmallFilmCard} from '../small-film-card/small-film-card';
 
-type Props = PropsWithChildren;
+type Props = PropsWithChildren<{
+  className?: string;
+}>;
 
-export function Catalog({children}: Props): JSX.Element {
+export function Catalog({children, className}: Props): JSX.Element {
+  const isRelatedFilms = !!className;
+
   return (
-    <section className="catalog">
+    <section className={`catalog ${className}`}>
       <h2 className="catalog__title visually-hidden">Catalog</h2>
+      {
+        isRelatedFilms && <h2 className="catalog__title">More like this</h2>
+      }
 
       {children}
 
