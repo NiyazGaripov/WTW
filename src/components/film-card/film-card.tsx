90047110234aa0557a4dc38f60745894591a43bf
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-09-30 09:24:52 +0300

    refactor: add props and replace static data to data from props

--- src/components/film-card/film-card.tsx
+++ src/components/film-card/film-card.tsx
@@ -1,12 +1,17 @@
 import {Header} from '../header/header';
 import {FilmShortDescription} from '../film-short-description/film-short-description';
 import {FilmInfo} from '../film-info/film-info';
+import {Film} from '../../types/film.type';
 
-export function FilmCard(): JSX.Element {
+type Props = {
+  film: Film
+};
+
+export function FilmCard({film}: Props): JSX.Element {
   return (
     <section className="film-card">
       <div className="film-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+        <img src={film.backgroundImage} alt={film.name}/>
       </div>
 
       <h1 className="visually-hidden">WTW</h1>
@@ -15,13 +20,13 @@ export function FilmCard(): JSX.Element {
 
       <div className="film-card__wrap">
         <FilmInfo
-          name='The Grand Budapest Hotel'
-          posterImage='img/the-grand-budapest-hotel-poster.jpg'
+          name={film.name}
+          posterImage={film.posterImage}
         >
           <FilmShortDescription
-            name='The Grand Budapest Hotel'
-            genre='Drama'
-            releaseYear='2014'
+            name={film.name}
+            genre={film.genre}
+            releaseYear={film.released}
           />
         </FilmInfo>
       </div>
