da609f1043d4a6e31ae0a6963cdc77d0415d6883
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-10 09:25:06 +0300

    refactor: add markup for header in add-review page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@ import {Logo} from '../logo/logo';
 export function Header(): JSX.Element {
   const isUserPage = false;
   const isLogin = false;
+  const isAddReview = false;
 
   return (
     <header className={`page-header film-card__head ${isUserPage && 'user-page__head'}`}>
@@ -11,6 +12,19 @@ export function Header(): JSX.Element {
       {/*TODO add condition for MyList and Login pages*/}
       {isUserPage && <h1 className="page-title user-page__title">My list <span className="user-page__film-count">9</span></h1>}
       {isLogin && <h1 className="page-title user-page__title">Sign in</h1>}
+      {
+        isAddReview &&
+        <nav className="breadcrumbs">
+          <ul className="breadcrumbs__list">
+            <li className="breadcrumbs__item">
+              <a href="film-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
+            </li>
+            <li className="breadcrumbs__item">
+              <a className="breadcrumbs__link">Add review</a>
+            </li>
+          </ul>
+        </nav>
+      }
 
       <ul className="user-block">
         <li className="user-block__item">
