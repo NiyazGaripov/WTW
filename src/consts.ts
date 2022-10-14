6500a152f1bc0ab224ab9a20999539da2e872a12
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-14 10:00:26 +0300

    refactor: rename key in enum

--- src/consts.ts
+++ src/consts.ts
@@ -47,7 +47,7 @@ export enum AppRoute {
   Main = '/',
   Login = '/login',
   MyList = '/mylist',
-  Movie = '/films/:id',
+  Film = '/films/:id',
   AddReview = '/films/:id/review',
   Player = '/player/:id',
   NotFound = '*',
