a2d3851b88bf2ce41f587ac2dee702372b814ae8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-17 22:52:45 +0300

    feat: create app routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,44 @@
+import {Route, Routes} from 'react-router-dom';
+import {AppRoute, films} from '../../consts';
 import {Main} from '../../pages/main/main';
+import {Login} from '../../pages/login/login';
+import {MyList} from '../../pages/my-list/my-list';
+import {Film} from '../../pages/film/film';
+import {AddReview} from '../../pages/add-review/add-review';
+import {Player} from '../../pages/player/player';
+import {NotFound} from '../../pages/not-found/not-found';
 
 export function App(): JSX.Element {
   return (
-    <Main/>
+    <Routes>
+      <Route
+        path={AppRoute.Main}
+        element={<Main/>}
+      />
+      <Route
+        path={AppRoute.Login}
+        element={<Login/>}
+      />
+      <Route
+        path={AppRoute.MyList}
+        element={<MyList/>}
+      />
+      <Route
+        path={AppRoute.Movie}
+        element={<Film film={films[0]}/>}
+      />
+      <Route
+        path={AppRoute.AddReview}
+        element={<AddReview name='Johnny English' backgroundImage='img/johnny-english.jpg'/>}
+      />
+      <Route
+        path={AppRoute.Player}
+        element={<Player/>}
+      />
+      <Route
+        path={AppRoute.NotFound}
+        element={<NotFound/>}
+      />
+    </Routes>
   );
 }
