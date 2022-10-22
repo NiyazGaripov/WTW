f1075398aeee1483c6a9664a2d3f2baf31e678d0
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-22 22:00:41 +0300

    feat: create component review-form

---
+++ src/components/review-form/review-form.tsx
@@ -0,0 +1,18 @@
+import {Rating} from '../rating/rating';
+
+export function ReviewForm(): JSX.Element {
+  return (
+    <div className="add-review">
+      <form action="#" className="add-review__form">
+        <Rating/>
+
+        <div className="add-review__text">
+          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+          <div className="add-review__submit">
+            <button className="add-review__btn" type="submit">Post</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
