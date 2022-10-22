40d039223c8bc5e9d01168a9afc9b61e80cb305f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-22 22:03:02 +0300

    feat: add prop isBigPoster for FilmInfo component

--- src/components/film-info/film-info.tsx
+++ src/components/film-info/film-info.tsx
@@ -3,12 +3,13 @@ import {PropsWithChildren} from 'react';
 type Props = PropsWithChildren<{
   name: string,
   posterImage: string,
+  isBigPoster?: boolean,
 }>;
 
-export function FilmInfo({children, name, posterImage}: Props): JSX.Element {
+export function FilmInfo({children, name, posterImage, isBigPoster = false}: Props): JSX.Element {
   return (
     <div className="film-card__info">
-      <div className="film-card__poster">
+      <div className={`film-card__poster ${isBigPoster && 'film-card__poster--big'}`}>
         <img src={posterImage} alt={name} width="218" height="327"/>
       </div>
 
