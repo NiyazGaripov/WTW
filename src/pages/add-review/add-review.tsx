d6eace55bd13772acffb5645107fc9a636f420c4
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-10-23 17:40:56 +0300

    refactor: add prop and ReviewForm component

--- src/pages/add-review/add-review.tsx
+++ src/pages/add-review/add-review.tsx
@@ -1,23 +1,32 @@
 import {SvgSprite} from '../../components/svg-sprite/svg-sprite';
 import {Header} from '../../components/header/header';
+import {ReviewForm} from '../../components/review-form/review-form';
 
 type Props = {
   name: string;
+  posterImage: string;
   backgroundImage: string;
 };
 
-export function AddReview({name, backgroundImage}: Props): JSX.Element {
+export function AddReview({name, posterImage, backgroundImage}: Props): JSX.Element {
   return (
     <>
       <SvgSprite/>
       <section className="film-card film-card--full">
-        <div className="film-card__bg">
-          <img src={backgroundImage} alt={name}/>
+        <div className="film-card__header">
+          <div className="film-card__bg">
+            <img src={backgroundImage} alt={name}/>
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <Header placeUse="add-review"/>
+
+          <div className="film-card__poster film-card__poster--small">
+            <img src={posterImage} alt={name} width="218" height="327"/>
+          </div>
         </div>
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <Header placeUse="add-review"/>
+        <ReviewForm/>
       </section>
     </>
   );
