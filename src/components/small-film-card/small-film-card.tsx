e92ea6aaec8621c200b4ae4e8468dea98b44c300
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-11-06 20:06:07 +0300

    refactor: add props and condition for render poster or video

--- src/components/small-film-card/small-film-card.tsx
+++ src/components/small-film-card/small-film-card.tsx
@@ -1,28 +1,36 @@
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../consts';
+import {VideoPlayer} from '../video-player/video-player';
 
 type Props = {
   name: string;
   posterImage: string;
+  isPlaying: boolean;
+  videoLink: string;
+  previewVideoLink: string;
   onCardEnter?: () => void;
   onCardLeave?: () => void;
 };
 
-export function SmallFilmCard({name, posterImage, onCardEnter, onCardLeave}: Props): JSX.Element {
+export function SmallFilmCard({name, posterImage, isPlaying, videoLink, previewVideoLink, onCardEnter, onCardLeave}: Props): JSX.Element {
   return (
     <article
       className="small-film-card catalog__films-card"
       onMouseEnter={onCardEnter}
       onMouseLeave={onCardLeave}
     >
-      <div className="small-film-card__image">
-        <img
-          src={posterImage}
-          alt={name}
-          width="280"
-          height="175"
-        />
-      </div>
+      {
+        isPlaying ?
+          <VideoPlayer isPlaying={isPlaying} videoLink={videoLink} previewVideoLink={previewVideoLink}/> :
+          <div className="small-film-card__image">
+            <img
+              src={posterImage}
+              alt={name}
+              width="280"
+              height="175"
+            />
+          </div>
+      }
       <h3 className="small-film-card__title">
         <Link to={AppRoute.Movie} className="small-film-card__link">{name}</Link>
       </h3>
