840d639c2bad1499969918f7289d2e38ed294e3e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-11-06 20:05:05 +0300

    feat: add prop siPlaying and use hook useEffect for play video

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -1,9 +1,27 @@
+import {useEffect, useRef} from 'react';
+
 type Props = {
+  isPlaying: boolean;
   videoLink: string;
   previewVideoLink: string;
 }
 
-export function VideoPlayer({videoLink, previewVideoLink}: Props): JSX.Element {
+export function VideoPlayer({isPlaying, videoLink, previewVideoLink}: Props): JSX.Element {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current === null) {
+      return;
+    }
+
+    if (isPlaying) {
+      videoRef.current.play();
+      return;
+    }
+
+    videoRef.current.pause();
+  }, [isPlaying]);
+
   return (
     <video
       src={videoLink}
