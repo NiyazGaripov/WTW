5116dc7825aa772ddffe3793c6045385fc3825db
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-11-09 18:11:36 +0300

    refactor: replace button to Link

--- src/components/controls/controls.tsx
+++ src/components/controls/controls.tsx
@@ -4,19 +4,27 @@ import {AppRoute} from '../../consts';
 export function Controls(): JSX.Element {
   return (
     <div className="film-card__buttons">
-      <button className="btn btn--play film-card__button" type="button">
+      <Link
+        to={AppRoute.Player}
+        className="btn btn--play film-card__button"
+      >
         <svg viewBox="0 0 19 19" width="19" height="19">
           <use xlinkHref="#play-s"></use>
         </svg>
         <span>Play</span>
-      </button>
-      <button className="btn btn--list film-card__button" type="button">
+      </Link>
+
+      <Link
+        to={AppRoute.MyList}
+        className="btn film-card__button"
+      >
         <svg viewBox="0 0 19 20" width="19" height="20">
           <use xlinkHref="#add"></use>
         </svg>
         <span>My list</span>
         <span className="film-card__count">9</span>
-      </button>
+      </Link>
+
       <Link to={AppRoute.AddReview} className="btn film-card__button">Add review</Link>
     </div>
   );
