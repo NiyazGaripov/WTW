18c9200fbfe974beed3c9952b23a1a0e0af389d7
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-12-02 20:19:29 +0300

    refactor: create function getFilms, add constants, add hook useEffect with dispatch filteredMoviesByGenre

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,22 +1,39 @@
-import {useSearchParams} from 'react-router-dom';
+import {useEffect} from 'react';
 import {SvgSprite} from '../../components/svg-sprite/svg-sprite';
 import {FilmCard} from '../../components/film-card/film-card';
 import {Catalog} from '../../components/catalog/catalog';
 import {Footer} from '../../components/footer/footer';
 import {GenresList} from '../../components/genres-list/genres-list';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import {filteredMoviesByGenre} from '../../store/action';
+import {Film} from '../../types/film.type';
 import {films} from '../../mocks/films';
 
+const getFilms = (genre: string, movies: Film[]): Film[] => {
+  if (genre === 'All genres') {
+    return movies;
+  }
+
+  return movies.filter((movie) => movie.genre === genre);
+};
+
 export function Main(): JSX.Element {
-  const [searchParams] = useSearchParams();
-  const activeGenre = searchParams.get('genre') || 'all';
+  const dispatch = useAppDispatch();
+  const {activeGenre} = useAppSelector((state) => state);
+  const filteredFilms = getFilms(activeGenre, films);
+  const film = filteredFilms[0];
+
+  useEffect(() => {
+    dispatch(filteredMoviesByGenre(filteredFilms));
+  }, [activeGenre]);
 
   return (
     <>
       <SvgSprite/>
-      <FilmCard film={films[0]}/>
+      {film && <FilmCard film={film}/>}
       <div className="page-content">
-        <Catalog films={films}>
-          <GenresList activeGenre={activeGenre}/>
+        <Catalog films={filteredFilms}>
+          <GenresList/>
         </Catalog>
         <Footer/>
       </div>
