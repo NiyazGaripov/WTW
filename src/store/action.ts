3155744dea8844d79acb33e3d6d690a415189e75
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-12-11 18:39:16 +0300

    feat: create action showMoreMovies

--- src/store/action.ts
+++ src/store/action.ts
@@ -3,3 +3,4 @@ import {Film} from '../types/film.type';
 
 export const changeGenre = createAction<string>('common/changeGenre');
 export const filteredMoviesByGenre = createAction<Film[]>('common/filteredMoviesByGenre');
+export const showMoreMovies = createAction<void>('common/showMoreMovies');
