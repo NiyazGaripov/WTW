39de3ec90b016c90def3fc618a2b7624914e8966
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-12-11 18:40:33 +0300

    feat: add numberOfFilmsShown in state, create case for action showMoreMovies, add reset numberOfFilmsShown

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,7 @@
 import {createReducer} from '@reduxjs/toolkit';
 import {AuthorizationStatus, Config} from '../consts';
 import {films} from '../mocks/films';
-import {changeGenre, filteredMoviesByGenre} from './action';
+import {changeGenre, filteredMoviesByGenre, showMoreMovies} from './action';
 import {Film} from '../types/film.type';
 
 type InitialState = {
@@ -9,6 +9,7 @@ type InitialState = {
   movies: Film[];
   genres: string[];
   activeGenre: string;
+  numberOfFilmsShown: number;
 }
 
 const initialState: InitialState = {
@@ -16,14 +17,19 @@ const initialState: InitialState = {
   movies: films,
   genres: ['All genres', ...new Set(films.map((movie) => movie.genre))],
   activeGenre: Config.DEFAULT_GENRE,
+  numberOfFilmsShown: Config.NUMBER_OF_FILMS_SHOWN,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeGenre, (state, action) => {
       state.activeGenre = action.payload;
+      state.numberOfFilmsShown = Config.NUMBER_OF_FILMS_SHOWN;
     })
     .addCase(filteredMoviesByGenre, (state, action) => {
       state.movies = action.payload;
+    })
+    .addCase(showMoreMovies, (state) => {
+      state.numberOfFilmsShown += Config.NUMBER_OF_FILMS_SHOWN;
     });
 });
