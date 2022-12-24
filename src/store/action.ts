192ced6ef0d659d0ca18dac141828229f057c5e4
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-12-24 14:50:27 +0300

    feat: create action requireAuthorization

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,10 @@
 import {createAction} from '@reduxjs/toolkit';
 import {Film} from '../types/film.type';
+import {AuthorizationStatus} from '../consts';
 
 export const changeGenre = createAction<string>('common/changeGenre');
 export const filteredMoviesByGenre = createAction<Film[]>('common/filteredMoviesByGenre');
 export const showMoreMovies = createAction<void>('common/showMoreMovies');
 export const resetNumberOfFilmsShown = createAction<void>('common/resetNumberOfFilmsShown');
 export const loadFilms = createAction<Film[]>('data/loadFilms');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
