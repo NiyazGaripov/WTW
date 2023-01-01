707a7ec895df3f18187fcc9b86a997c373933db8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-01 19:30:35 +0300

    feat: create action setGenres

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,6 +2,7 @@ import {createAction} from '@reduxjs/toolkit';
 import {Film} from '../types/film.type';
 import {AuthorizationStatus, DataLoadingStatus} from '../consts';
 
+export const setGenres = createAction<string[]>('common/setGenres');
 export const changeGenre = createAction<string>('common/changeGenre');
 export const filteredMoviesByGenre = createAction<Film[]>('common/filteredMoviesByGenre');
 export const showMoreMovies = createAction<void>('common/showMoreMovies');
