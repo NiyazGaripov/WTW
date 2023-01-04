a9d8b2e1738ff98e895af1d4e82e1032b96c86e6
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-04 16:51:44 +0300

    refactor: remove props and add promoFilm from state

--- src/components/film-card/film-card.tsx
+++ src/components/film-card/film-card.tsx
@@ -1,17 +1,16 @@
 import {Header} from '../header/header';
 import {FilmShortDescription} from '../film-short-description/film-short-description';
 import {FilmInfo} from '../film-info/film-info';
-import {Film} from '../../types/film.type';
+import {useAppSelector} from '../../hooks';
 
-type Props = {
-  film: Film;
-};
+export function FilmCard(): JSX.Element {
+  const promoFilm = useAppSelector((state) => state.promoFilm);
+  const {name, posterImage, backgroundImage, genre, released} = promoFilm;
 
-export function FilmCard({film}: Props): JSX.Element {
   return (
     <section className="film-card">
       <div className="film-card__bg">
-        <img src={film.backgroundImage} alt={film.name}/>
+        <img src={backgroundImage} alt={name}/>
       </div>
 
       <h1 className="visually-hidden">WTW</h1>
@@ -20,13 +19,13 @@ export function FilmCard({film}: Props): JSX.Element {
 
       <div className="film-card__wrap">
         <FilmInfo
-          name={film.name}
-          posterImage={film.posterImage}
+          name={name}
+          posterImage={posterImage}
         >
           <FilmShortDescription
-            name={film.name}
-            genre={film.genre}
-            releaseYear={film.released}
+            name={name}
+            genre={genre}
+            releaseYear={released}
           />
         </FilmInfo>
       </div>
