7b7be63800afa8e744471a10803d5510baf9ed3f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-06 17:43:36 +0300

    refactor: change type of constant filteredMoviesByGenre

--- src/store/action.ts
+++ src/store/action.ts
@@ -4,7 +4,7 @@ import {AuthorizationStatus, DataLoadingStatus} from '../consts';
 
 export const setGenres = createAction<string[]>('common/setGenres');
 export const changeGenre = createAction<string>('common/changeGenre');
-export const filteredMoviesByGenre = createAction<Film[]>('common/filteredMoviesByGenre');
+export const filteredMoviesByGenre = createAction<string>('common/filteredMoviesByGenre');
 export const showMoreMovies = createAction<void>('common/showMoreMovies');
 export const resetNumberOfFilmsShown = createAction<void>('common/resetNumberOfFilmsShown');
 export const loadFilms = createAction<Film[]>('data/loadFilms');
