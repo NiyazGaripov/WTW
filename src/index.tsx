30b2f1226c0b37173ffab915fd6d8344b59e2621
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-07 10:57:12 +0300

    feat: dispatch api-actions

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import {App} from './components/app/app';
+import {checkAuthAction, fetchFavoriteFilmsAction, fetchFilmsAction, fetchPromoFilmAction} from './store/api-actions';
+
+store.dispatch(checkAuthAction());
+store.dispatch(fetchFilmsAction());
+store.dispatch(fetchPromoFilmAction());
+store.dispatch(fetchFavoriteFilmsAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
