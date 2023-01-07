4aa164ba2a39bc19e134926d9cde02f152aa68e2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-07 11:00:36 +0300

    refactor: change constants filteredMovies and showedFilms, remove dispatch setGenres, add dependencies of useEffects, fix small bugs

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -5,44 +5,32 @@ import {Catalog} from '../../components/catalog/catalog';
 import {Footer} from '../../components/footer/footer';
 import {GenresList} from '../../components/genres-list/genres-list';
 import {useAppDispatch, useAppSelector} from '../../hooks';
-import {filteredMoviesByGenre, resetNumberOfFilmsShown, setGenres} from '../../store/action';
-import {Film} from '../../types/film.type';
+import {filteredMoviesByGenre, resetNumberOfFilmsShown} from '../../store/action';
 import {ShowMore} from '../../components/show-more/show-more';
-
-const getFilteredFilms = (genre: string, movies: Film[]): Film[] => {
-  if (genre === 'All genres') {
-    return movies;
-  }
-
-  return movies.filter((movie) => movie.genre === genre);
-};
-
-const getGenres = (movies: Film[]): string[] => ['All genres', ...new Set(movies.map((movie) => movie.genre))];
 
 export function Main(): JSX.Element {
   const dispatch = useAppDispatch();
   const movies = useAppSelector((state) => state.movies);
   const activeGenre = useAppSelector((state) => state.activeGenre);
   const numberOfFilmsShown = useAppSelector((state) => state.numberOfFilmsShown);
-  const filteredFilms = getFilteredFilms(activeGenre, movies);
-  const showedFilms = filteredFilms.slice(0, numberOfFilmsShown);
+  const filteredMovies = useAppSelector((state) => state.filteredMovies);
+  const showedFilms = filteredMovies.slice(0, numberOfFilmsShown);
 
   useEffect(() => {
     dispatch(resetNumberOfFilmsShown());
-    dispatch(filteredMoviesByGenre(filteredFilms));
-    dispatch(setGenres(getGenres(movies)));
-  }, [activeGenre]);
+    dispatch(filteredMoviesByGenre(activeGenre));
+  }, [dispatch, activeGenre, movies]);
 
   return (
     <>
       <SvgSprite/>
-      {FilmCard}
+      <FilmCard/>
       <div className="page-content">
         {
           movies.length ?
             <Catalog films={showedFilms}>
               <GenresList/>
-              {((filteredFilms.length - showedFilms.length) > 0) && <ShowMore/>}
+              {((filteredMovies.length - showedFilms.length) > 0) && <ShowMore/>}
             </Catalog> :
             <div>Loading...</div>
         }
