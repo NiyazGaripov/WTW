476702c4b6834dacf16c0bf9dbf12487cb67b95b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-11 09:05:38 +0300

    refactor: use type Validator

--- src/hooks/useFormField.tsx
+++ src/hooks/useFormField.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, useCallback, useState} from 'react';
-import {useValidation} from './useValidation';
+import {useValidation, Validator} from './useValidation';
 
 type FormField = {
   value: string
@@ -9,7 +9,7 @@ type FormField = {
   valid: Record<string, string | number | boolean>,
 }
 
-export function useFormField(initialValue: string, validators: Record<string, string | number | boolean>): FormField {
+export function useFormField(initialValue: string, validators: Validator): FormField {
   const [value, setValue] = useState(initialValue);
   const [isDirty, setDirty] = useState(false);
   const valid = useValidation(value, validators);
