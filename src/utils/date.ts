5b45eeccb59a6a565e0d7f877e202690af7d9c9f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-27 12:35:27 +0300

    feat: create function formatCommentDate

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,3 +2,5 @@ import dayjs from 'dayjs';
 import durationPlugin from 'dayjs/plugin/duration';
 
 dayjs.extend(durationPlugin);
+
+export const formatCommentDate = (date: string) => dayjs(date).format('MMMM DD, YYYY');
