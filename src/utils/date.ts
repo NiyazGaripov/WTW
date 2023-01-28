842665d756be6e7c17cbd04faf77d6af7c9b1b8a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-01-28 18:38:00 +0300

    feat: create function getFilmDuration and add return type

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,4 +3,19 @@ import durationPlugin from 'dayjs/plugin/duration';
 
 dayjs.extend(durationPlugin);
 
-export const formatCommentDate = (date: string) => dayjs(date).format('MMMM DD, YYYY');
+export const formatCommentDate = (date: string): string => dayjs(date).format('MMMM DD, YYYY');
+
+export const getFilmDuration = (duration: number): string => {
+  const filmDuration = dayjs.duration(duration, 'minutes');
+  const hours: number = filmDuration.hours();
+  const minutes: number = filmDuration.minutes();
+
+  let normalizeHours: string = hours > 0 ? `${hours}h` : '';
+  const normalizeMinutes: string = minutes > 0 ? `${minutes}m` : '';
+
+  if (hours && minutes) {
+    normalizeHours += ' ';
+  }
+
+  return normalizeHours + normalizeMinutes;
+};
